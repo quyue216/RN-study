c840f68d666e2e00d4ffbcf5d6d64fbca92d028e
quyue <10676114@qq.com>
2025-03-06 22:08:20 +0800

    feat: 实现todos

--- pages/Index.tsx
+++ pages/Index.tsx
@@ -1,44 +1,93 @@
-import {
-    View,
-    Text,
-    StyleSheet,
-    ScrollView
-} from 'react-native';
+import React from 'react';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
 import Header from './Header/Header';
-import TodoList from "./List/LIst"
-import Footer from "./Footer/Footer";import { useEffect, useState } from 'react';
-;
+import TodoList from './List/LIst';
+import Footer from './Footer/Footer';
+import { useState} from 'react';
 
-type Todo ={
-  id:number,
-  content:string,
-  checked:boolean
+type Todo = {
+  id: number;
+  content: string;
+  checked: boolean;
 };
 
-export default function Index(){
+export default function Index() {
   // 保存todo列表
-   const [todos,setTodos] =  useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([
+    {id: 1, content: 'todo1', checked: false},
+    {id: 2, content: 'todo2', checked: true},
+    {id: 3, content: 'todo3', checked: false},
+  ]);
 
-    return (
-      <View style={styles.app}>
-        <View style={styles.todosContainer}>
-          <Header></Header>
-          <TodoList></TodoList>
-          <Footer></Footer>
-        </View>
+  const completedCount = todos.filter(todo => todo.checked).length;
+
+  const addTodo = (todo: Todo) => {
+    const nextTodos = [todo, ...todos];
+    setTodos(nextTodos);
+  };
+
+  const deleteTodo = (id: number) => {
+    const nextTodos = todos.filter(todo => todo.id !== id);
+    setTodos(nextTodos);
+  };
+
+  const toggleTodo = (id: number) => {
+    const nextTodos = todos.slice();
+
+    const todo = nextTodos.find(todo => todo.id === id);
+
+    if (todo) {
+      todo.checked = !todo.checked;
+    }
+
+    setTodos(nextTodos);
+  };
+
+  //全选
+  const toggleAll = (result: true | false) => {
+    const nextTodos = todos.map(todo => {
+      todo.checked = result;
+      return todo;
+    });
+    setTodos(nextTodos);
+  };
+
+  const deleteAllChecked = () => {
+    const nextTodos = todos.filter(todo => !todo.checked);
+    setTodos(nextTodos);
+  };
+
+  let footerProps = {
+    toggleAll,
+    deleteAllChecked,
+    todoCount: todos.length,
+  };
+
+  return (
+    <View style={styles.app}>
+      <View style={styles.todosContainer}>
+        <Header addTodo={addTodo}></Header>
+        <TodoList
+          todos={todos}
+          toggleTodo={toggleTodo}
+          deleteTodo={deleteTodo}></TodoList>
+        <Footer completedCount={completedCount} {...footerProps}></Footer>
       </View>
-    );
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   app: {
     flex: 1,
     padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  todosContainer:{
-    borderWidth:1,
-    borderColor:'#ccc',
-    borderRadius:5,
-    padding:10,
-  }
+  todosContainer: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+  },
 });
--- pages/Item/Item.tsx
+++ pages/Item/Item.tsx
@@ -1,53 +1,71 @@
-import { View ,Text,Button, StyleSheet} from "react-native";
-import BouncyCheckbox from "react-native-bouncy-checkbox";
+import {View, Text, Button, StyleSheet} from 'react-native';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import {CheckBox} from '@rneui/themed';
 
+type Todo = {
+  id: number;
+  content: string;
+  checked: boolean;
+};
 
+export default function Item({
+  todo,
+  toggleTodo,
+  deleteTodo
+}: {
+  todo: Todo;
+  toggleTodo: (id: number) => void;
+    deleteTodo: (id: number) => void;
+}) {
 
-export default function Item() {
+  const checkedView = (
+    <View style={todoItem.btnWrap}>
+        {/* 添加一个确认的弹框 */}
+      <Button title="删除" color="red" onPress={() => deleteTodo(todo.id)}/>
+    </View>
+  );
+
   return (
-     <View style={todoItem.todoItem}>
-        <View style={todoItem.todoName}>
-            <BouncyCheckbox
-                size={25}
-                fillColor="#ccc"
-                unFillColor="#CCC"
-                text="Custom Checkbox"
-                iconStyle={{ borderColor: "#ccc" }}
-                innerIconStyle={{ borderWidth: 2 }}
-                textStyle={{ fontFamily: "JosefinSans-Regular" }}
-                onPress={(isChecked: boolean) => {console.log(isChecked)}}
-                    />
-        </View>
-        
-            <View style={todoItem.btnWrap}>
-                <Button  title="删除" color="red"/>
-            </View>
-       
-     </View>
-  )
+    <View style={todoItem.todoItem}>
+      <View style={todoItem.todoName}>
+        <CheckBox
+          checked={todo.checked}
+          onPress={() => {
+            toggleTodo(todo.id);
+          }}
+          iconType="material-community"
+          checkedIcon="checkbox-marked"
+          uncheckedIcon="checkbox-blank-outline"
+          checkedColor="red"
+          containerStyle={{marginLeft: -10}}
+          title={todo.content}
+        />
+      </View>
+      {todo.checked && checkedView}
+    </View>
+  );
 }
 
-
 const todoItem = StyleSheet.create({
-    todoItem:{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        alignItems:'center',
-        paddingHorizontal:10,
-        width:'100%',
-        height:50,
-        borderBottomWidth:1,
-        borderBottomColor:'#ccc',
-    },
-    btnWrap:{
-        height:'100%',
-        flexDirection:'row',
-        alignItems:'center',
-    },
-    todoName:{
-        width:'35%',
-        height:'100%',
-        flexDirection:'row',
-        alignItems:'center',
-    }
+  todoItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    width: '100%',
+    height: 50,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  btnWrap: {
+    height: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  todoName: {
+    width: '35%',
+    height: '110%',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
 });
