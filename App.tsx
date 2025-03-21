8cec9ffa24c77db3e26c1002852a988dfec61217
quyue <10676114@qq.com>
2025-03-21 11:09:29 +0800

    feat: 搭建项目目录

--- App.tsx
+++ App.tsx
@@ -5,36 +5,34 @@
  * @format
  */
 import React, {useEffect, useState} from 'react';
-import {
-  View
-} from 'react-native';
+import {View, Text} from 'react-native';
+import SystemNavigationBar from 'react-native-system-navigation-bar';
 import Orientation from 'react-native-orientation-locker';
-import Index from "./pages/Index";
+import Index from './src/Index';
 
-function App(){
-  const [showTodos,setShowTodos] = useState(false)
+// console.log(SystemNavigationBar,"------");
+
+function App() {
+  const [showTodos, setShowTodos] = useState(true);
 
   useEffect(() => {
     // 锁定屏幕为横向
     Orientation.lockToLandscape();
+   
     setTimeout(() => {
-      setShowTodos(true)
+      setShowTodos(true);
     }, 1000);
+   
+    console.log('SystemNavigationBar',SystemNavigationBar);
+    
+
     // 在组件卸载时解锁屏幕方向
     return () => {
       Orientation.unlockAllOrientations();
     };
-    
   }, []);
 
-
-    return (
-          <View style={{flex: 1}}>
-           {showTodos && <Index />}
-        </View>
-    );
-  
+  return <View style={{flex: 1}}>{showTodos && <Index />}</View>;
 }
-
 
 export default App;
---
+++ src/Index.tsx
@@ -0,0 +1,13 @@
+import {View,Text} from 'react-native'
+
+
+
+export default function Index() {
+  return (
+    <View>
+        <Text>
+            Index
+        </Text>
+    </View>
+  )
+}
